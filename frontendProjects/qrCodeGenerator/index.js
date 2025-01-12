8bce46f3014404f812874442b69f84d80e1f4176
Aditya Dhiman <info@adityadhiman.in>
2025-01-12 20:04:43 +0530

    added frontend projects

--- frontendProjects/qrCodeGenerator/index.js
+++ frontendProjects/qrCodeGenerator/index.js
@@ -1,13 +1,23 @@
 const img = document.querySelector("img");
 const button = document.querySelector("button");
-const text = document.querySelector("input");
+const input = document.querySelector("input");
 
 button.addEventListener("click", () => {
-  img.setAttribute(
-    "src",
-    "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" +
-      text.value
-  );
-  img.classList.add("show");
-  console.log(text.value);
+  try {
+    if (input.value) {
+      img.setAttribute(
+        "src",
+        "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=" +
+          input.value
+      );
+      img.classList.add("show");
+      input.value = "";
+    } else {
+      alert("Please enter a value to generate a QR code");
+    }
+  } catch (error) {
+    alert("Something went wrong! Please try after sometime.");
+    img.setAttribute("src", "");
+    input.value = "";
+  }
 });
